7b17d45d6a03aeda51f9142397446af9d01c8496
Alexander Reinthal <email@reinthal.me>
2025-08-11 11:36:55 +0200

    refactor: docker command fail2

--- modules/build-tarball.nix
+++ modules/build-tarball.nix
@@ -37,8 +37,8 @@ with lib; let
       nix = {
         settings.experimental-features = [ "nix-command" "flakes" ];
         extraOptions = ''
-         extra-substituters = https://devenv.cachix.org
-         extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+         extra-substituters = "https://devenv.cachix.org"
+         extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
        '';
       };
 
